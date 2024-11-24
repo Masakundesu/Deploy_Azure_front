a1cc4c7fcdeddad03882ff58881aac53b4c770be
Masakundesu <shibamasa1003@gmail.com>
2024-11-24 22:33:32 +0900

    Update index.js
    localhost:5000を下記に変更
    https://tech0-gen-8-step3-testapp-py2-20.azurewebsites.net

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ const MapComponent = () => {
 
   const fetchMarkers = useCallback(async () => {
     try {
-      const response = await axios.get('http://localhost:5000/api/markers');
+      const response = await axios.get('https://tech0-gen-8-step3-testapp-py2-20.azurewebsites.net/api/markers');
       setMarkers(response.data);
     } catch (error) {
       console.error('Error fetching markers:', error);
@@ -46,7 +46,7 @@ export default function Home() {
   // GETリクエストを送信
   const [getResponse, setGetResponse] = useState('');
   const handleGetRequest = async () => {
-    const res = await fetch('http://localhost:5000/api/hello', {
+    const res = await fetch('https://tech0-gen-8-step3-testapp-py2-20.azurewebsites.net/api/hello', {
       method: 'GET',
     });
     const data = await res.json();
@@ -66,7 +66,7 @@ export default function Home() {
   const handleIdRequest = async (e) => {
     e.preventDefault();
 
-    const res = await fetch(`http://localhost:5000/api/multiply/${id}`, {
+    const res = await fetch(`https://tech0-gen-8-step3-testapp-py2-20.azurewebsites.net/api/multiply/${id}`, {
       method: 'GET',
     });
     const data = await res.json();
@@ -87,7 +87,7 @@ export default function Home() {
     //入力されたデータをコンソールに表示
     console.log("入力情報:", input);
 
-    const res = await fetch('http://localhost:5000/api/echo', {
+    const res = await fetch('https://tech0-gen-8-step3-testapp-py2-20.azurewebsites.net/api/echo', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
